ac978aa3be97b733f7f4a8bbe6d98beb542cf0ec
yaaman1002 <166231007+yaaman1002@users.noreply.github.com>
2024-05-28 09:48:10 +0000

    [finish]2-13

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "homes#top"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'homes/about', as: 'about'
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] 
 end
