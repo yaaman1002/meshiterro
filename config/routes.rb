0625ffc60427fb912c978fad201722acab01da45
yaaman1002 <166231007+yaaman1002@users.noreply.github.com>
2024-06-17 14:11:34 +0000

    [add]管理者機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  scope module: :public do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about', as: 'about'
   resources :post_images, only:[:new, :create, :index, :show, :destroy] do
     resource :favorite, only:[:create, :destroy]
     resources :post_comments, only:[:create, :destroy]
+   end
+    resources :users, only: [:show, :edit, :update]
   end
-  resources :users, only:[:show, :edit, :update]
+    devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+  
+  namespace :admin do
+    get 'dashboards', to: 'dashboarde#index'
+    resources :users, only:[:destroy]
+  end
 end
